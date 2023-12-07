32451b231d0049dc841a6f97c8d759af4b9c81ec
Emre Ercelebi <30380456+emreercelebi@users.noreply.github.com>
2023-12-06 19:17:35 -0500

    skipping day 5 part 2

--- day5.py
+++ day5.py
@@ -1,6 +1,6 @@
 from readFile import read_file
 
-lines = read_file("inputs/day5.txt")
+lines = read_file("inputs/day5_test.txt")
 
 class Mapping:
     def __init__(self, destination, source, range):
@@ -44,42 +44,7 @@ def part1():
                     val = mapped_val
                     break
         result = min(result, val)
-    return result
-
-def part2():
-    seed_ranges = [int(e) for e in lines[0][7:].split()]
-    
-    map_lists = []
-
-    i = 2
-    while i < len(lines):
-        line = lines[i]
-        if not line[0].isdigit():
-            i += 1
-            map_list = []
-            while i < len(lines) and len(lines[i]) > 0 and lines[i][0].isdigit():
-                parts = lines[i].split()
-                map_list.append(Mapping(int(parts[0]), int(parts[1]), int(parts[2])))
-                i += 1
-            i += 1
-            map_lists.append(map_list)
-
-    result = 99999999999
-    for i in range(0, len(seed_ranges), 2):
-        for seed in range(int(seed_ranges[i]), int(seed_ranges[i + 1])):
-            val = int(seed)
-            for map_list in map_lists:
-                for mapping in map_list:
-                    mapped_val = mapping.get_value(val)
-                    if mapped_val != val:
-                        val = mapped_val
-                        break
-            result = min(result, val)
-    return result
-
-
-        
+    return result  
 
 
 print("part 1:", part1())
-print("part 2:", part2())
